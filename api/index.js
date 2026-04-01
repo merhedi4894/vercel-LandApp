87cefc1034fd4308c957da152fada3ac1b7fd0c2
Mehedi Hasan <mehedi24.info@gmail.com>
2026-04-01 22:07:24 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const { Profile, LandData } = require('./models/DataSchema');
+const { Profile, LandData } = require('../models/DataSchema');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
